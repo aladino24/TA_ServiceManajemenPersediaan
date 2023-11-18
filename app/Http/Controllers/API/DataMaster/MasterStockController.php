ae1378679f7e651c81c5c94d3b02e521fe647ef3
komodo22 <aladinozulmar@gmail.com>
2023-11-18 12:09:57 +0700

    CRUD Stock

--- app/Http/Controllers/API/DataMaster/MasterStockController.php
+++ app/Http/Controllers/API/DataMaster/MasterStockController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\API\DataMaster;
 
+use App\Helpers\Convert;
 use App\Http\Controllers\Controller;
 use App\Models\Stock;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 
 class MasterStockController extends Controller
 {
@@ -16,4 +19,164 @@ class MasterStockController extends Controller
             'data' => $stock
         ], 200);
     }
+
+    public function createStock(Request $request){
+        // validator
+        $validator = Validator::make($request->all(), [
+            'fc_stockcode' => 'required',
+            'fc_barcode' => 'required',
+        ]);
+
+        $user = Session::get('user');
+        $branch = $user['user']['branch'];
+        // response error validator
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+
+        $request->request->add(['fc_branch' => $branch]);
+        if(empty($request->type)){
+            $cek_data = Stock::where([
+                'fc_stockcode' => $request->fc_stockcode,
+                'deleted_at' => null,
+            ])->withTrashed()->count();
+
+            if($cek_data > 0){
+                return [
+                    'status' => 300,
+                    'message' => 'Oops! Insert gagal karena data sudah ditemukan didalam sistem kami'
+                ];
+            }
+        }
+
+        $request->merge(['fn_reorderlevel' => doubleval($request->fn_reorderlevel) ]);
+        $request->merge(['fn_maxonhand' => doubleval($request->fn_maxonhand) ]);
+        $request->merge(['fm_cogs' => doubleval($request->fm_cogs) ]);
+        $request->merge(['fm_purchase' => doubleval($request->fm_purchase) ]);
+        $request->merge(['fm_salesprice' => doubleval($request->fm_salesprice) ]);
+
+        $request->merge(['fm_purchase' => doubleval($request->fm_purchase) ]);
+        $request->merge(['fm_salesprice' => doubleval($request->fm_salesprice) ]);
+        $request->merge(['fm_disc_pr' => doubleval($request->fm_disc_pr) ]);
+        $request->merge(['fm_disc_rp' => doubleval($request->fm_disc_rp) ]);
+        $request->merge(['fm_time_disc_rp' => doubleval($request->fm_time_disc_rp) ]);
+        $request->merge(['fm_time_disc_pr' => doubleval($request->fm_time_disc_pr) ]);
+        $request->merge(['fm_price_default' => doubleval($request->fm_price_default) ]);
+        $request->merge(['fm_price_distributor' => doubleval($request->fm_price_distributor) ]);
+        $request->merge(['fm_price_project' => doubleval($request->fm_price_project) ]);
+        $request->merge(['fm_price_dealer' => doubleval($request->fm_price_dealer) ]);
+        $request->merge(['fm_price_enduser' => doubleval($request->fm_price_enduser) ]);
+
+
+        // insert
+        try {
+            $stock = Stock::create($request->all());
+            return response()->json([
+                'status' => 200,
+                'message' => 'Berhasil menambahkan data stock',
+                'data' => $stock
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Oops! Terjadi kesalahan saat menambahkan data stock',
+                'data' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    
+    public function updateStock(Request $request){
+        // validator
+        $validator = Validator::make($request->all(), [
+            'fc_stockcode' => 'required',
+            'fc_barcode' => 'required',
+        ]);
+
+        $user = Session::get('user');
+        $branch = $user['user']['branch'];
+        // response error validator
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+
+        $request->request->add(['fc_branch' => $branch]);
+
+        $request->merge(['fn_reorderlevel' => doubleval($request->fn_reorderlevel) ]);
+        $request->merge(['fn_maxonhand' => doubleval($request->fn_maxonhand) ]);
+        $request->merge(['fm_cogs' => doubleval($request->fm_cogs) ]);
+        $request->merge(['fm_purchase' => doubleval($request->fm_purchase) ]);
+        $request->merge(['fm_salesprice' => doubleval($request->fm_salesprice) ]);
+
+        $request->merge(['fm_purchase' => doubleval($request->fm_purchase) ]);
+        $request->merge(['fm_salesprice' => doubleval($request->fm_salesprice) ]);
+        $request->merge(['fm_disc_pr' => doubleval($request->fm_disc_pr) ]);
+        $request->merge(['fm_disc_rp' => doubleval($request->fm_disc_rp) ]);
+        $request->merge(['fm_time_disc_rp' => doubleval($request->fm_time_disc_rp) ]);
+        $request->merge(['fm_time_disc_pr' => doubleval($request->fm_time_disc_pr) ]);
+        $request->merge(['fm_price_default' => doubleval($request->fm_price_default) ]);
+        $request->merge(['fm_price_distributor' => doubleval($request->fm_price_distributor) ]);
+        $request->merge(['fm_price_project' => doubleval($request->fm_price_project) ]);
+
+        $request->merge(['fm_price_dealer' => doubleval($request->fm_price_dealer) ]);
+        $request->merge(['fm_price_enduser' => doubleval($request->fm_price_enduser) ]);
+
+        // update
+        try {
+            $stock = Stock::where('fc_stockcode', $request->fc_stockcode)
+            ->where('fc_barcode', $request->fc_barcode)
+            ->update($request->all());
+            return response()->json([
+                'status' => 200,
+                'message' => 'Berhasil mengubah data stock',
+                'data' => $stock
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Oops! Terjadi kesalahan saat mengubah data stock',
+                'data' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function deleteStock($fc_stockcode, $fc_barcode){
+        $user = Session::get('user');
+        $branch = $user['user']['branch'];
+
+        // Decode fc_stockcode and fc_barcode
+        $decode_fc_stockcode = base64_decode($fc_stockcode);
+        $decode_fc_barcode = base64_decode($fc_barcode);
+
+        try {
+            $delete = Stock::where([
+                'fc_stockcode' => $decode_fc_stockcode,
+                'fc_barcode' => $decode_fc_barcode,
+                'fc_branch' => $branch
+            ])->delete();
+
+            if ($delete) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Berhasil menghapus data stock',
+                    'data' => $delete
+                ], 200);
+            } else {
+                // Handle the case where the delete operation did not delete any records
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'Data stock tidak ditemukan atau tidak dapat dihapus',
+                    'data' => null
+                ], 404);
+            }
+        } catch (\Throwable $th) {
+            // Handle other exceptions
+            return response()->json([
+                'status' => 500,
+                'message' => 'Oops! Terjadi kesalahan saat menghapus data stock',
+                'data' => $th->getMessage()
+            ], 500);
+        }
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -22,8 +22,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => 'check-authentication'], function () {
     Route::group(['prefix' => 'master'], function () {
+
+        // master stock
         Route::get('stock', [MasterStockController::class, 'getStock']);
+        Route::post('stock', [MasterStockController::class, 'createStock']);
+        Route::put('stock', [MasterStockController::class, 'updateStock']);
+        Route::delete('stock/{fc_stockcode}/{fc_barcode}', [MasterStockController::class, 'deleteStock']);
         Route::get('brand', [MasterBrandController::class, 'getBrand']);
+       
         // Route::get('stock/{id}', 'API\DataMaster\MasterStockController@getStockById');
         // Route::post('stock', 'API\DataMaster\MasterStockController@createStock');
         // Route::put('stock/{id}', 'API\DataMaster\MasterStockController@updateStock');
