91aa9cbe1132b5650c8334dcc5184e1b4e09a6c9
komodo22 <aladinozulmar@gmail.com>
2024-07-03 17:15:48 +0700

    Tambahan

--- app/Models/Scanqr.php
+++ app/Models/Scanqr.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Scanqr extends Model
+class ScanQr extends Model
 {
     use HasFactory;
     protected static $logAttributes = ["*"];
